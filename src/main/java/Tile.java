c4476ea94511e5d45b9972311d08618fcfdf84e0
Samuel King <snks081018@gmail.com>
2021-08-23 17:16:41 +0100

    Third time's the charm

---
+++ src/main/java/Tile.java
@@ -0,0 +1,66 @@
+public class Tile {
+    // attributes
+
+    private boolean isBomb;
+    private int surroundingBombs;
+    private boolean uncovered;
+    private boolean flagged;
+    private int xCoordinate;
+    private int yCoordinate;
+
+    // constructors
+
+    public Tile(int x,int y){
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+        float randomFloat = (float) Math.random();
+        float bombRatio = 0.3f;
+        if(randomFloat <= bombRatio){
+            isBomb = true;}
+        else{
+            isBomb = false;}
+        uncovered = true;
+    }
+
+    // methods
+
+    public void checkSurroundingBombs(Tile[][] someTile,int a,int b){
+        for(int i = -1; i <= 1; i++){
+            for(int j = -1; j <= 1; j++){
+                if(a+i <= 0 || a+i >= 10 || b+j <= 0 || b+j >= 10){
+                    System.out.println("out of bounds at ("+(a+i)+","+(b+j)+")");
+                }else{
+                    //someTile = Tile[a+i][b+j];
+                    if(this.isBomb){
+                        this.surroundingBombs++;
+                    }}
+            }
+        }
+
+    }
+
+
+    public int getSurroundingBombs(){
+        return surroundingBombs;}
+
+    public void setSurroundingBombs( int surroundingBombCounter){
+        this.surroundingBombs = surroundingBombCounter;}
+
+    public boolean getIsBomb(){
+        return isBomb;}
+
+    public void setIsBomb( boolean bombState){
+        this.isBomb = bombState;}
+
+    public String toString(){
+        if(!uncovered){
+            return "[ ]";
+        }
+        else if(isBomb){
+            return "[*]";
+        }
+        else{
+            return "["+surroundingBombs+"]";
+        }
+    }
+}
