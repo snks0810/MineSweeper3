fe55c06a923568fa6f65c773bd3572dc382e1b6b
snks0810 <89077247+snks0810@users.noreply.github.com>
2021-08-24 12:09:43 +0100

    Add files via upload
    Added 2 more tests with the state of flags and bombs

--- MineSweeper3/src/test/java/MineSweeper3/TileTest.java
+++ MineSweeper3/src/test/java/MineSweeper3/TileTest.java
@@ -9,11 +9,29 @@ import org.junit.jupiter.api.DisplayName;
 public class TileTest {
 
     @Test
-    @DisplayName("some test")
-    public void someTest(){
+    @DisplayName("bomb counter test")
+    public void bombCounterTest(){
         Tile[][] someTile = new Tile[0][0];
         someTile[0][0].setSurroundingBombs(2);
         assertEquals(2,someTile[0][0].getSurroundingBombs(),
                 "failed to hard code the surrounding bomb counter");
+    }
+
+    @Test
+    @DisplayName("bomb state test")
+    public void bombStateTest(){
+        Tile[][] someTile = new Tile[0][0];
+        someTile[0][0].setIsBomb(true);
+        assertEquals(true,someTile[0][0].getIsBomb(),
+                "failed to hard code the state of a bomb");
+    }
+
+    @Test
+    @DisplayName("flag state test")
+    public void flagStateTest(){
+        Tile[][] someTile = new Tile[0][0];
+        someTile[0][0].setFlagged(true);
+        assertEquals(true,someTile[0][0].getFlagged(),
+                "failed to hard code the state of a flag");
     }
 }
