a9a8fb43bcafbfce18c4351d0105491113524a9c
snks0810 <89077247+snks0810@users.noreply.github.com>
2021-08-24 16:22:49 +0100

    Add files via upload

--- MineSweeper3/src/test/java/MineSweeper3/TileTest.java
+++ MineSweeper3/src/test/java/MineSweeper3/TileTest.java
@@ -1,7 +1,6 @@
 package MineSweeper3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
